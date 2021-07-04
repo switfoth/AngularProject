308c0d2243c764e410181b4ab496dbb718a61635
Seth <switfoth@gmail.com>
2021-07-04 17:40:19 -0500

    Button component created

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title = 'Armedia Angular Project';
+
   constructor() { }
 
   ngOnInit(): void {
