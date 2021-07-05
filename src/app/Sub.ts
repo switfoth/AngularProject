eb7688733ed2d030b7991b6ec63c6b06a8755077
Seth <switfoth@gmail.com>
2021-07-05 12:08:17 -0500

    Added mock subs, began creation of Sub interface

---
+++ src/app/Sub.ts
@@ -0,0 +1,9 @@
+// email address (required)
+// dropdown to selected Basic, Advanced, and Pro subscriptions with advanced as the default
+// required password field with the parameters: 8 characters long with at least one regular and one special character
+// password confirmation field - if the passwords do not match, submit should not be clickable
+// submit button and clear button - submit button has to verify all fields have valid data, clear button is active at all times.
+export interface Sub {
+    id: number,
+
+}
---
+++ src/app/mock-subs.ts
@@ -0,0 +1,17 @@
+export const SUBS = [
+    {
+        id: 1,
+        email: 'main@123.com',
+        subscription: 'Advanced'
+    },
+    {
+        id: 2,
+        email: 'richie@bucks.com',
+        subscription: 'Pro'
+    },
+    {
+        id: 3,
+        email: 'indie@artist.com',
+        subscription: 'Basic'
+    }
+]
